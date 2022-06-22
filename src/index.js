6025eb30cc7ab78ed0d1ba25434bd0a5577917cb
oldfish1996 <2318587805@qq.com>
2022-06-22 22:06:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import React from "react"
 import {createRoot} from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 const root = createRoot(document.getElementById('root'))
-root.render(<BrowserRouter><App/></BrowserRouter>)
+root.render(<HashRouter><App/></HashRouter>)
